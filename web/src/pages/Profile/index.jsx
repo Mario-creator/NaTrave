244b474c6d4e2a72e948372f01e4559ee4694270
Mario Silva <mcesarss@hotmail.com>
2022-10-16 12:35:26 -0300

    finish_front

--- web/src/pages/Profile/index.jsx
+++ web/src/pages/Profile/index.jsx
@@ -1,23 +1,24 @@
 import { Icon, Card } from '~/components'
 
-export const Dashboard = () => (
+export const Profile = () => (
     <>
         <header className="bg-red-500 text-white p-4">
             <div className="container max-w-3xl flex justify-between text-white">
                 <img src="/img/logo-fundo-vermelho.svg" className="w-28 md:w-40"/>
-                <a href="/profile">
-                    <Icon name="profile" className="w-10"/>
-                </a>
             </div>
         </header>
         <main className='space-y-6' >
             <section id='header' className="bg-red-500 text-white">
                 <div className='container max-w-3xl space-y-2 p-4'>
-                    <span>Olá Bruno</span>
-                    <h3 className='text-2xl font-bold'>Qual é o seu palpite?</h3>
+                    <a href="/dashboard">
+                        <Icon name="back" className="w-10"/>
+                    </a>
+                    
+                    <h3 className='text-2xl font-bold'>Bruno Bertolini</h3>
                 </div>
             </section>
             <section id='content' className='p-4 container max-w-3xl space-y-4'>
+                <h2 className='text-red-500 text-xl font-bold' >Seus palpites</h2>
 
                 <div className='p-4 flex space-x-4 items-center justify-center'>
                     <Icon name="arrowLeft" className="w-6 text-red-500"/>
