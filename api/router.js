d324f9ce307e3d5b044ca628f0cfca6cede47a57
Mario Silva <mcesarss@hotmail.com>
2022-10-18 22:54:09 -0300

    dataBase

--- api/router.js
+++ api/router.js
@@ -1,11 +1,18 @@
 import Router from '@koa/router'
 
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
 export const router = new Router()
 
-router.get('/', async ctx => {
-    ctx.body = { ola: "Mundo"}
+router.get('/users', async ctx => {
+    ctx.body = []
 })
 
-router.get('/users', async ctx => {
-    ctx.body = { ola: "Users"}
+router.post('/users', async ctx => {
+    const user = {
+    username: ctx.request.body.username
+    }
+
+    ctx.body = user
 })
