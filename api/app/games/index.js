697c5c0fa2f9e0de32529587bc43747de13f1346
Mario Silva <mcesarss@hotmail.com>
2022-10-25 21:37:24 -0300

    login

--- api/app/games/index.js
+++ api/app/games/index.js
@@ -1,9 +1,20 @@
 import { PrismaClient } from '@prisma/client'
+import { addDays, formatISO } from 'date-fns'
+
 const prisma = new PrismaClient()
 
 export const list = async ctx => {
+    const currentDate = ctx.request.query.gameTime
+
+    const where = currentDate ? {
+        gameTime: {
+            gte: currentDate,
+            lt: formatISO(addDays(new Date(currentDate), 1))
+        }
+    } : {}   
+
     try {
-        const games = await prisma.game.findMany()
+        const games = await prisma.game.findMany({ where })
         ctx.body = games
         ctx.status = 200
     } catch (error) {
@@ -11,5 +22,5 @@ export const list = async ctx => {
         ctx.body = error
         ctx.status = 500
     }
-
+    
 }
